e605159adfd85efdf1042ffe3958ab4767e619a1
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-10-06 22:16:52 +0700

    TodoList

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
-    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
-    <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
+    <v-text-field label="What needs to be done" v-model="newTodo" @keyup.enter="addTodo"></v-text-field>
+    <transition-group name="slide-fade">
     <div v-for="(todo, index) in todosFiltered" :key="todo.id" class="todo-item">
       <div class="todo-item-left">
-        <input type="checkbox" v-model="todo.completed">
+        <v-checkbox v-model="todo.completed" class="mx-0"></v-checkbox>
         <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label" :class="{ completed : todo.completed }">{{ todo.title }}</div>
         <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
       </div>
       <div class="remove-item" @click="removeTodo(index)">
-        &times;
+       <v-icon color="red">mdi-delete</v-icon>
       </div>
     </div>
     </transition-group>
 
     <div class="extra-container">
-      <div><label><input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos"> Check All</label></div>
+      <div>
+        <label><input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos"> Check All</label>
+      </div>
       <div>{{ remaining }} items left</div>
     </div>
 
@@ -31,8 +33,13 @@
         <button v-if="showClearCompletedButton" @click="clearCompleted">Clear Completed</button>
         </transition>
       </div>
-
     </div>
+     <br>
+    <button @click="show = !show"> Credit </button>
+    <transition name="bounce">
+      <p v-if="show" class="my-5"> Create by Tinnakorn </p>
+    </transition>
+
   </div>
 </template>
 
@@ -41,6 +48,8 @@ export default {
   name: 'todo-list',
   data () {
     return {
+      show: false,
+      checkbox1: false,
       newTodo: '',
       idForTodo: 3,
       beforeEditCache: '',
@@ -121,6 +130,7 @@ export default {
       this.todos.splice(index, 1)
     },
     checkAllTodos() {
+      console.log(event.target.checked)
       this.todos.forEach((todo) => todo.completed = event.target.checked)
     },
     clearCompleted() {
@@ -131,7 +141,6 @@ export default {
 </script>
 
 <style lang="scss">
-  @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
   .todo-input {
     width: 100%;
     padding: 10px 18px;
@@ -209,5 +218,32 @@ export default {
   }
   .fade-enter, .fade-leave-to {
     opacity: 0;
+  }
+  .slide-fade-enter-active {
+    transition: all .3s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to {
+    transform: translateX(10px);
+    opacity: 0;
+  }
+  .bounce-enter-active {
+    animation: bounce-in .5s;
+  }
+  .bounce-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
   }
 </style>
